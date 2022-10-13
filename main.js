44fbdebe7a7266060006aa69e56c152dd1c0d2e3
gitHubMIH <114809703+gitHubMIH@users.noreply.github.com>
2022-10-13 18:52:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function setup() {
+  canvas=createCanvas(388,440);
+  canvas.center();
+}
+function draw() {
+
+}
+function takeSnapshot() {
+  save("fotoDeBigode.png");
+}
